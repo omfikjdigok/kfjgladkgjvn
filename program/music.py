fcf088eb86009e250b5b628ac624daa202337ed7
omfikjdigok <102236492+omfikjdigok@users.noreply.github.com>
2022-03-23 17:30:11 +0300

    Update music.py

--- program/music.py
+++ program/music.py
@@ -50,7 +50,7 @@ async def ytdl(link):
         return 0, stderr.decode()
 
 
-@Client.on_message(command(["مريم شغلي","/play","تشغيل","/mplay", f"/mplay@{BOT_USERNAME}"]) & other_filters)
+@Client.on_message(command(["ريفان شغلي","/play","تشغيل","/mplay", f"/mplay@{BOT_USERNAME}"]) & other_filters)
 async def play(c: Client, m: Message):
     replied = m.reply_to_message
     chat_id = m.chat.id
